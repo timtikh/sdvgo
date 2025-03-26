f2fdd28554da41ade52abcce38da5e13a930c21c
timtikh <alfertikhonov@yandex.ru>
2025-03-26 10:04:31 +0300

    linter

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:sdvgo/app.dart';
 import 'package:provider/provider.dart';
 import 'package:sdvgo/core/data/mock_user_model.dart';
-import 'package:sdvgo/core/domain/user_model.dart';
 
 void main() {
   runApp(ChangeNotifierProvider(create: (context) => mockUserModel, child: const MyApp()));
