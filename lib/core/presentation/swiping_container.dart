f4db5ebb44bb692c1e497c2ccdc58da8db4a42da
rogalikkkkkk <dmitriy.zharkov.2001@yandex.ru>
2025-03-31 18:29:16 +0300

    add swiping container (need check with tiktok)

---
+++ lib/core/presentation/swiping_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+const _swipeThreshold = 50.0;
+
+class SwipingContainer extends StatefulWidget {
+  const SwipingContainer({super.key, required this.children});
+
+  final List<Widget> children;
+
+  @override
+  State<SwipingContainer> createState() => _SwipingContainerState();
+}
+
+class _SwipingContainerState extends State<SwipingContainer> {
+  int currentWidgetIndex = 0;
+  late Offset startPosition;
+
+  void handleSwipe(Offset delta) {
+    if (delta.dx.abs() < delta.dy.abs()) {
+      return;
+    }
+    if (delta.dx.abs() < _swipeThreshold) {
+      return;
+    }
+    if (delta.dx < 0) {
+      currentWidgetIndex = (currentWidgetIndex + 1) % widget.children.length;
+    } else {
+      currentWidgetIndex = (currentWidgetIndex - 1) % widget.children.length;
+    }
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: widget.children[currentWidgetIndex],
+      onHorizontalDragStart: (details) {
+        startPosition = details.globalPosition;
+      },
+      onHorizontalDragEnd: (details) {
+        final delta = details.globalPosition - startPosition;
+        handleSwipe(delta);
+      },
+    );
+  }
+}
--- lib/features/home/presentation/upper_controller.dart
+++ lib/features/home/presentation/upper_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sdvgo/core/presentation/swiping_container.dart';
 
 // TODO: он типо должен хэндлить логику переключения между списком вижетов
 // TODO:  а в идеале вообще быть унаследован от одного и того же типа
@@ -11,8 +12,15 @@ class UpperController extends StatefulWidget {
 }
 
 class _UpperControllerState extends State<UpperController> {
+  List<Widget> children = List.generate(4, (i) {
+    return ColoredBox(
+      color: Colors.pink.shade100,
+      child: Center(child: Text('$i')),
+    );
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container(color:Colors.pink.shade100,);
+    return SwipingContainer(children: children);
   }
 }
