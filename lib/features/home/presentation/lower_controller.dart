528a8b2030f6caadeb13f9e131c7ba89b6b1c294
Dmitry <mityadb@yandex.ru>
2025-04-03 11:45:45 +0300

    add logic for clicks in game

--- lib/features/home/presentation/lower_controller.dart
+++ lib/features/home/presentation/lower_controller.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sdvgo/core/di/app_scope.dart';
 import 'package:sdvgo/core/presentation/swiping_container.dart';
 import 'package:sdvgo/features/scooter_game/presentation/scooter_game_widget.dart';
-
+import 'package:yx_scope_flutter/yx_scope_flutter.dart';
 // TODO: он типо должен хэндлить логику переключения между списком вижетов
 // TODO:  а в идеале вообще быть унаследован от одного и того же типа
 
@@ -16,18 +17,26 @@ class _LowerControllerState extends State<LowerController> {
   @override
   Widget build(BuildContext context) {
     // TODO: add callback method realisation
-    return ColoredBox(
-      color: Colors.pink.shade100,
-      child: SwipingContainer(
-        children: [
-          ScooterGameWidget(
-            addClicks: () {},
+    return ScopeBuilder<AppScopeContainer>.withPlaceholder(
+      builder: (context, scope) {
+        final userStatisticsCubit = scope.userStatisticsCubitDep.get;
+
+        return ColoredBox(
+          color: Colors.pink.shade100,
+          child: SwipingContainer(
+            children: [
+              ScooterGameWidget(
+                addClicks: () {
+                  userStatisticsCubit.increaseClicksCountByValue(13);
+                },
+              ),
+              Center(
+                child: Text('Screen 2'),
+              ),
+            ],
           ),
-          Center(
-            child: Text('Screen 2'),
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
