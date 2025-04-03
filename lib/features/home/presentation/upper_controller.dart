b161dd1735ce77ae6ca5c23c17aebd3b4a772f37
timtikh <alfertikhonov@yandex.ru>
2025-04-03 18:18:42 +0300

    analyzer redone

--- lib/features/home/presentation/upper_controller.dart
+++ lib/features/home/presentation/upper_controller.dart
@@ -3,7 +3,6 @@ import 'dart:core'; // For using URIs
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:sdvgo/core/presentation/swiping_container.dart';
 import 'package:sdvgo/features/tiktok/data/repositories/tiktok_repository_impl.dart';
 import 'package:sdvgo/features/tiktok/presentation/tiktok_screen.dart';
